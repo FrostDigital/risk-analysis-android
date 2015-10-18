b4e0e2a7bccab0de8039adf1b8a19a3c25fea0fb
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2015-10-18 23:30:22 +0200

    Adding Prio hint overlay texts

--- RiskAnalysis/app/src/main/java/se/frostdigital/riskanalysis/RiskAnalysisLegendView.java
+++ RiskAnalysis/app/src/main/java/se/frostdigital/riskanalysis/RiskAnalysisLegendView.java
@@ -21,11 +21,13 @@ public class RiskAnalysisLegendView extends View {
 
     private String[] xTitles, yTitles;
     private String mXAxisTitle, mYAxisTitle;
+    private String mPrioLowText, mPrioMediumText, mPrioHighText;
+    private RectF mPrioLowTextArea, mPrioMediumTextArea, mPrioHighTextArea;
     private float mAxisOffset;
     private int mWidth, mHeight;
     private RectF[] xAreas, yAreas;
     private RectF xAxisTitleArea, yAxisTitleArea;
-    private Paint mTextPaint, mAxisTitlePaint;
+    private Paint mTextPaint, mAxisTitlePaint, mPrioHintPaint;
 
     private Rect mReusableBounds;
     private Paint fitTextPaint;
@@ -47,6 +49,7 @@ public class RiskAnalysisLegendView extends View {
 
     private void initTitles() {
         if (isInEditMode()) {
+            //Used only in preview
             xTitles = new String[]{"1","2","3","4","5","6"};
             yTitles = new String[]{"1","2","3","4","5","6"};
         } else {
@@ -55,6 +58,11 @@ public class RiskAnalysisLegendView extends View {
         }
         mXAxisTitle = getResources().getString(R.string.x_axis_title);
         mYAxisTitle = getResources().getString(R.string.y_axis_title);
+
+        mPrioLowText = getResources().getString(R.string.prio_hint_low);
+        mPrioMediumText = getResources().getString(R.string.prio_hint_medium);
+        mPrioHighText = getResources().getString(R.string.prio_hint_high);
+
     }
 
     private void initPaints() {
@@ -65,12 +73,18 @@ public class RiskAnalysisLegendView extends View {
         mTextPaint.setTextAlign(Paint.Align.CENTER);
 
         mAxisTitlePaint = new Paint(mTextPaint);
+        mPrioHintPaint = new Paint(mTextPaint);
         fitTextPaint = new Paint(mTextPaint);
     }
+
+    ////
+    //// Init areas methods
+    ////
 
     private void initTitlesAreasWithWidthAndHeight(int width, int height) {
         initXTitlesAreasWithWidthAndHeight(width, height);
         initYTitlesAreasWithWidthAndHeight(width, height);
+        initPrioHintAreas(width, height);
     }
 
     private void initXTitlesAreasWithWidthAndHeight(int width, int height) {
@@ -129,6 +143,27 @@ public class RiskAnalysisLegendView extends View {
         mAxisTitlePaint.setTextSize(Math.min(mAxisTitlePaint.getTextSize(), getTextSizeForTextInRect(mYAxisTitle, yAxisTitleArea)));
     }
 
+    private void initPrioHintAreas(int width, int height) {
+        float thirdWidth = (width - mAxisOffset) / 3.0f;
+        float thirdHeight = (height - mAxisOffset) / 3.0f;
+        //Bottom left without offset - Low
+        mPrioLowTextArea = new RectF(mAxisOffset, thirdHeight * 2, mAxisOffset + thirdWidth, height - mAxisOffset);
+        //Middle without offset - Medium
+        mPrioMediumTextArea = new RectF(mAxisOffset + thirdWidth, thirdHeight, mAxisOffset + 2*thirdWidth, 2 * thirdHeight);
+        //Top Right - High
+        mPrioHighTextArea = new RectF(width - thirdWidth, 0, width, thirdHeight);
+
+        //Calculate best text size
+        float textSize = getTextSizeForTextInRect(mPrioLowText, mPrioLowTextArea);
+        textSize = Math.min(textSize, getTextSizeForTextInRect(mPrioMediumText, mPrioLowTextArea));
+        textSize = Math.min(textSize, getTextSizeForTextInRect(mPrioHighText, mPrioLowTextArea));
+        mPrioHintPaint.setTextSize(textSize);
+    }
+
+    ////
+    //// Draw methods
+    ////
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -139,6 +174,7 @@ public class RiskAnalysisLegendView extends View {
         }
         drawXAxisTitles(canvas);
         drawYAxisTitles(canvas);
+        drawPrioHints(canvas);
     }
 
     private void drawXAxisTitles(Canvas canvas) {
@@ -168,6 +204,32 @@ public class RiskAnalysisLegendView extends View {
             drawTextInRectWithPaint(canvas, yTitles[i], yAreas[i], mTextPaint);
         }
         canvas.restore();
+    }
+
+    private void drawPrioHints(Canvas canvas) {
+        float tan = (float)mHeight / (float)mWidth;
+        float angle = (float) (Math.atan(tan) / Math.PI * 180.0f);
+
+        if (mPrioLowTextArea != null) {
+            canvas.save();
+            canvas.rotate(angle, mPrioLowTextArea.centerX(), mPrioLowTextArea.centerY());
+            drawTextInRectWithPaint(canvas, mPrioLowText, mPrioLowTextArea, mPrioHintPaint);
+            canvas.restore();
+        }
+
+        if (mPrioMediumTextArea != null) {
+            canvas.save();
+            canvas.rotate(angle, mPrioMediumTextArea.centerX(), mPrioMediumTextArea.centerY());
+            drawTextInRectWithPaint(canvas, mPrioMediumText, mPrioMediumTextArea, mPrioHintPaint);
+            canvas.restore();
+        }
+
+        if (mPrioHighTextArea != null) {
+            canvas.save();
+            canvas.rotate(angle, mPrioHighTextArea.centerX(), mPrioHighTextArea.centerY());
+            drawTextInRectWithPaint(canvas, mPrioHighText, mPrioHighTextArea, mPrioHintPaint);
+            canvas.restore();
+        }
     }
 
     private void drawTextInRectWithPaint(Canvas canvas, String text, RectF rect, Paint paint) {
